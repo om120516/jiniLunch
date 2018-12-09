27ccf1fb8aa942c14a4e2935bc1e6f6324a1bd0b
jh910 <jh910@BIRD-JINNICOM>
2018-12-09 18:29:24 +0900

    기본세팅, 인코딩, 모델생성

--- jiniLunch/src/main/java/com/jini/jiniLunch/HomeController.java
+++ jiniLunch/src/main/java/com/jini/jiniLunch/HomeController.java
@@ -18,10 +18,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
+/*	
+	*//**
 	 * Simply selects the home view to render by returning its name.
-	 */
+	 *//*
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
@@ -34,6 +34,13 @@ public class HomeController {
 		model.addAttribute("serverTime", formattedDate );
 		
 		return "home";
+	}*/
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		
+		model.addAttribute("text", "hello JINI!" );
+		
+		return "index.jsp";
 	}
-	
 }
